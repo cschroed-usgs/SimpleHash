17a3894caeb327de2afb74443d4f1ab973fb763e
Carl Schroedl <cschroedl@usgs.gov>
2015-08-11 13:23:38 -0500

    Cleanup

--- src/test/java/gov/usgs/cida/simplehash/SimpleHashTest.java
+++ src/test/java/gov/usgs/cida/simplehash/SimpleHashTest.java
@@ -6,17 +6,9 @@
 package gov.usgs.cida.simplehash;
 
 import java.security.NoSuchAlgorithmException;
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
-/**
- *
- * @author cschroed
- */
 public class SimpleHashTest {
 	
 	/**
